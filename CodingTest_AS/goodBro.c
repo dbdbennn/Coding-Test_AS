7a779bb4392b9f02810760989b28dc083fcac465
Yu Jung Eun <0hh.0553@gmail.com>
2022-09-01 19:29:35 +0900

    의좋은 형제

---
+++ CodingTest_AS/goodBro.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void main() {
+	int jw, sw;
+	int day;
+	scanf("%d %d", &jw, &sw);
+	scanf("%d", &day);
+
+	for (int i = 1; i <= day; i++) {
+		if (i % 2 != 0) {
+			if (jw % 2 == 0) {
+				jw = jw / 2;
+				sw = sw + jw;
+			}
+			else {
+				jw = jw / 2;
+				sw = jw + sw + 1;
+			}
+		}
+		else {
+			if (sw % 2 == 0) {
+				sw = sw / 2;
+				jw = jw + sw;
+			}
+			else {
+				sw = sw / 2;
+				jw = jw + sw + 1;
+			}
+		}
+	}
+	printf("%d %d", jw, sw);
+}
